38f8f8a4371c0f41808fb1f673de3e08d3e4bb20
Adedipupo <bayonleolaluwa@gmail.com>
2020-11-04 01:51:28 +0100

    wip

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-var books = document.querySelectorAll("#book-list li .name");
+const books = document.querySelector("#book-list");
 
-Array.from(books).forEach(function (book) {
-  book.textContent += "(Otunba)";
-});
+books.innerHTML = "<h2> Books and more books </h2>"
