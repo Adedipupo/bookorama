2cf58fe7d52f6cf5fb59a573bf937dfca1b17ca4
Adedipupo <bayonleolaluwa@gmail.com>
2020-11-05 09:58:27 +0100

    wip

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 
-const list = document.querySelector('#book-list ul');
+const ul = document.querySelector('#book-list ul');
 
-list.addEventListener('click', function(e){
+//delete book
+ul.addEventListener('click', function(e){
     if (e.target.className === 'delete') {
         const li = e.target.parentNode;
-        list.removeChild(li);
+        ul.removeChild(li);
     }
+});
+
+//add book
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log(e.target.value);
 })
